05c29e3568c388e0b4c35abbb7e36d25b7774918
Vitor Santos <vmvs007@gmail.com>
2023-10-13 14:52:30 +0100

    FichaPratica02

--- pythonProject/FichaPratica02/Ex_06.py
+++ pythonProject/FichaPratica02/Ex_06.py
@@ -7,11 +7,11 @@ num2=int(input("Insira um número: "))
 
 # caso num1 maior que num2
 if(num1>num2):
-    print(num2, num1)
+    print(num1, num2)
 
 # caso num2 maior que num1
 if(num2>num1):
-    print(num1, num2)
+    print(num2, num1)
 
 # caso sejam iguais
 if(num1==num2):
