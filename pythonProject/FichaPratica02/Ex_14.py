7f1063bd196398b0e435513778f5a64cd04f8ca1
Vitor Santos <vmvs007@gmail.com>
2023-10-16 12:25:06 +0100

    FichaPratica02

--- pythonProject/FichaPratica02/Ex_14.py
+++ pythonProject/FichaPratica02/Ex_14.py
@@ -1,2 +1,29 @@
+# ler numero 1
+a = int(input("Insira numero 1: "))
 
-# ler ...
+# ler numero 2
+b = int(input("Insira numero 2: "))
+
+# ler numero 3
+c = int(input("Insira numero 3: "))
+
+# a mais pequeno
+if (a < b and a < c):
+    if (b < c):
+        print(a, b, c)
+    else:
+        print(a, c, b)
+
+# b mais pequeno
+if (b < a and b < c):
+    if (a < c):
+        print(b, a, c)
+    else:
+        print(b, c, a)
+
+# c mais pequeno
+if (c < a and c < b):
+    if (a < b):
+        print(c, a, b)
+    else:
+        print(c, b, a)
